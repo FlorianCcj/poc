66cf13744a63c412bbd5687d48d6ec8d312e1bab
florianccj <palmade.florian@gmail.com>
2017-10-30 09:42:31 +0100

    [+] [poc tns, ng] tuto presque fini flemme d aller au bout

--- ng-native/app/components/detail/detail.component.ts
+++ ng-native/app/components/detail/detail.component.ts
@@ -1,4 +1,7 @@
-import {{Component}} from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterExtensions} from 'nativescript-angular/router'
+import {ModalDialogParams} from 'nativescript-angular/directives/dialogs';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Component({
 	moduleId: module.id,
@@ -6,5 +9,38 @@ import {{Component}} from '@angular/core';
 	templateUrl: 'detail.component.html'
 })
 export class DetailComponent {
-	
+	public items$: BehaviorSubject<Array<any>> = new BehaviorSubject([]); 
+
+	constructor(private router: RouterExtensions, private params: ModalDialogParams) {
+		console.log(this.params.context.msg);
+		let items: any[] = [
+			{title: 'NativeScript'},
+			{title: 'Angular'},
+			{title: 'TypeScript'},
+			{title: 'JavaScript'},
+		];
+		let cnt = 0;
+		let timer = setInterval(() => {
+			if ( cnt < 4 ) {
+				this.items$.next([...this.items$.getValue(), items[cnt]]);
+				cnt++;
+			} else {
+				clearInterval(timer);
+			}
+		}, 1000);
+	}
+
+	public onItemTap(e) {
+		for (let key in e) {
+			console.log(`${key}: ${e[key]}`);
+		}
+	}
+
+	public goBack() {
+		this.router.back();
+	}
+
+	public close() {
+		this.params.closeCallBack('closed the ModalDialogOptions.')
+	}
 }
--- ng-native/app/components/home/home.component.ts
+++ ng-native/app/components/home/home.component.ts
@@ -3,7 +3,12 @@ import {{Component}} from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'home',
-	templateUrl: 'home.component.html'
+	templateUrl: 'home.component.html',
+	style: [`
+		.active {
+			color: red;
+		}
+	`]
 })
 export class HomeComponent {
 	
